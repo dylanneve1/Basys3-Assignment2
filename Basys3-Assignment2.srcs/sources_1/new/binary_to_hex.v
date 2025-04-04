4b53dc93de74ea5ac92cf3241245ca08346497f7
Dylan Neve <dylanneve1@gmail.com>
2025-04-04 11:11:14 +0100

    Broke t hex

--- Basys3-Assignment2.srcs/sources_1/new/binary_to_hex.v
+++ Basys3-Assignment2.srcs/sources_1/new/binary_to_hex.v
@@ -6,11 +6,34 @@ module binary_to_hex(input [15:0] binary, output [3:0] hex3, hex2, hex1, hex0);
     wire [3:0] bcd2 = binary [11:8];
     wire [3:0] bcd3 = binary [15:12];
 
-    // TODO
-    // Implement Binary to Decimal converter using HEX
+    reg [3:0] bcd0_next, bcd1_next, bcd2_next, bcd3_next;
 
-    assign hex0 = bcd0;
-    assign hex1 = bcd1;
-    assign hex2 = bcd2;
-    assign hex3 = bcd3;
+    // 3-digit bcd counter
+    always @* begin
+        // default: keep the previous value
+        bcd0_next = bcd0;
+        bcd1_next = bcd1;
+        bcd2_next = bcd2;
+        bcd3_next = bcd3;
+        if (bcd0_next == 4'ha) begin
+            bcd0_next = 4'h0;
+            bcd1_next = bcd1_next + 4'h1;
+            if (bcd1_next == 4'ha) begin
+                bcd1_next = 4'h0;
+                bcd2_next = bcd2_next + 4'h1;
+                if (bcd2_next == 4'ha) begin
+                    bcd2_next = 4'h0;
+                    bcd3_next = bcd3_next + 4'h1;
+                    if (bcd3_next == 4'ha) begin
+                        bcd3_next = 4'h9;
+                    end
+                end
+            end
+        end
+   end
+
+    assign hex0 = bcd0_next;
+    assign hex1 = bcd1_next;
+    assign hex2 = bcd2_next;
+    assign hex3 = bcd3_next;
 endmodule
